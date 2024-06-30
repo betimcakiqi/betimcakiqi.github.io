aeb7b492cab9157c8fd217420f319a861f4853d8
bigbutaa <betimcakiqiii@gmail.com>
2024-07-01 01:15:09 +0200

    classes and resized icons

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -14,13 +14,13 @@ class FooterComponent extends HTMLElement {
         <link rel="stylesheet" href="./components/footer/footer.css">
         <footer>
             <div class="social-media">
-                <a href="https://www.linkedin.com/in/betim-cakiqi-60919b217/" target="_blank" rel="noopener noreferrer"><img
+                <a href="https://www.linkedin.com/in/betim-cakiqi-60919b217/" target="_blank" rel="noopener noreferrer"><img class="social-media-icons"
                         src="./assets/icons/social-media-icons/linkedin-icon.svg" alt="LinkedIn"></a>
-                <a href="https://www.instagram.com/betimcakiqii/" target="_blank" rel="noopener noreferrer"><img
+                <a href="https://www.instagram.com/betimcakiqii/" target="_blank" rel="noopener noreferrer"><img class="social-media-icons"
                         src="./assets/icons/social-media-icons/instagram-icon.svg" alt="Instagram"></a>
-                <a href="https://www.facebook.com/betimcakiqii/" target="_blank" rel="noopener noreferrer"><img
+                <a href="https://www.facebook.com/betimcakiqii/" target="_blank" rel="noopener noreferrer"><img class="social-media-icons"
                         src="./assets/icons/social-media-icons/facebook-icon.svg" alt="Facebook"></a>
-                <a href="https://x.com/betiimc" target="_blank" rel="noopener noreferrer"><img
+                <a href="https://x.com/betiimc" target="_blank" rel="noopener noreferrer"><img class="social-media-icons"
                         src="./assets/icons/social-media-icons/twitter-icon.svg" alt="Twitter"></a>
             </div>
             <span>
